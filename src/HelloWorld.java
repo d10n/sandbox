6764df587617b6c298ce28cd00bd60db101c9302
d10n <david@bitinvert.com>
2012-10-11 16:33:24 -0400

    simple Hello World program

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		f\u006fr(int $:"ÄĸŔŔŠ$ĀŠŬŔĴ$D@螤@H".to\u0043har\u0041rray())
+		Syst\u0065m.out/*\u002a\u002f.print\u0028(char)(\u002f*
+        \u002a/(($>>2)-~22)+((0-~0)>>'<'>>1)-('\''/'.')));/**/
+	}
+}
